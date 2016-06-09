508abb9e3b8f5977ec5cfda01aafb67ec55ae732
Damien Guihal <dguihal@gmail.com>
2016-06-09 23:58:24 +0200

    Add user authentication support

--- userHandler.go
+++ userHandler.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/boltdb/bolt"
 	"golang.org/x/crypto/bcrypt"
@@ -16,18 +17,17 @@ type User struct {
 	Id             uint64
 	Login          string
 	HashedPassword []byte
+	salt           []byte
 }
 
 const (
 	noError           = iota
 	internalDbError   = iota
+	internalError     = iota
 	userAlreadyExists = iota
+	userDoesNotExists = iota
+	wrongPassword     = iota
 )
-
-type userDBError struct {
-	Code uint
-	Err  error
-}
 
 type userHandler struct {
 	db *bolt.DB
@@ -40,9 +40,9 @@ func newUserHandler(db *bolt.DB) (u *userHandler) {
 	return
 }
 
-func (u *userHandler) AddUser(login string, password string) (err userDBError) {
-	var errCode uint = 0
-	error := u.db.Batch(func(tx *bolt.Tx) error {
+func (u *userHandler) AddUser(login string, password string) (errCode uint, err error) {
+
+	err = u.db.Batch(func(tx *bolt.Tx) error {
 		b, err := tx.CreateBucketIfNotExists([]byte(usersBucketName))
 		if err != nil {
 			errCode = internalDbError
@@ -67,6 +67,7 @@ func (u *userHandler) AddUser(login string, password string) (err userDBError) {
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 		fmt.Println("AddUser", password, hashedPassword)
 		if err != nil {
+			errCode = internalError
 			return err
 		}
 
@@ -74,20 +75,43 @@ func (u *userHandler) AddUser(login string, password string) (err userDBError) {
 
 		buf, err := json.Marshal(user)
 		if err != nil {
+			errCode = internalError
 			return err
 		}
 
 		err = b.Put(itob(user.Id), buf)
 
+		errCode = noError
 		return nil
 	})
 
-	ue := userDBError{Code: noError, Err: nil}
-	if error != nil {
-		ue.Code = errCode
-		ue.Err = error
-	}
-	return ue
+	return
+}
+
+func (u *userHandler) AuthUser(login string, password string) (errCode uint, err error) {
+
+	err = u.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(usersBucketName))
+
+		c := b.Cursor()
+		user := User{}
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			json.Unmarshal(v, &user)
+
+			if user.Login == login {
+				err = bcrypt.CompareHashAndPassword(user.HashedPassword, []byte(password))
+				if err != nil {
+					errCode = wrongPassword
+				}
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	return
 }
 
 func (u *userHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -101,10 +125,21 @@ func (u *userHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 		fmt.Println("POST", loginAttr, passwdAttr)
 
-		dbErr := u.AddUser(loginAttr, passwdAttr)
-		if dbErr.Code != noError {
+		var errCode uint = noError
+		err := errors.New("")
+
+		if strings.HasSuffix(r.URL.Path, "add") {
+			errCode, err = u.AddUser(loginAttr, passwdAttr)
+		} else if strings.HasSuffix(r.URL.Path, "login") {
+			errCode, err = u.AuthUser(loginAttr, passwdAttr)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		if errCode != noError {
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(dbErr.Err.Error()))
+			w.Write([]byte(err.Error()))
 		} else {
 			w.WriteHeader(http.StatusOK)
 		}
