d9e37b79759915873fdc2487cc117d704b2f0ad8
Damien Guihal <dguihal@restlet.com>
2016-06-23 15:43:31 +0200

    Add token based auth for admin operations

--- adminhandler.go
+++ adminhandler.go
@@ -27,4 +27,14 @@ func NewAdminHandler(db *bolt.DB) (a *AdminHandler) {
 }
 
 func (a *AdminHandler) ServeHTTP(w http.ResponseWriter, rq *http.Request) {
+	reqAdminToken := rq.Header.Get("Token-Id")
+	if !a.checkAdminToken(reqAdminToken) {
+		w.WriteHeader(http.StatusUnauthorized)
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func (a *AdminHandler) checkAdminToken(token string) bool {
+	return token == a.adminToken
 }
