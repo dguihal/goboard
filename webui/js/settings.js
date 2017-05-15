96240ac1d4051b1bd464f999e023dab8a23cc73a
Damien Guihal <dguihal@gmail.com>
2017-05-16 00:19:32 +0200

    Some style cleanups and make swagger file dynamic

---
+++ webui/js/settings.js
@@ -0,0 +1,11 @@
+var MAX_POSTS = 50;
+var PINI_REFRESH_MS = 10000;
+
+var BASE_PATH = "";
+
+var BACKEND_URL = BASE_PATH + "/backend/json?last=%i";
+
+var TOTOZ_DEFAULT_SERVER = "http://totoz.eu/img";
+
+var SWAGGER_BASE_PATH = "/swagger";
+var SWAGGER_FILE_NAME = "swagger.yaml";
