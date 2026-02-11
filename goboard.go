e5030471b7db7686b23ef7bf653adb0a6d139f3f
Damien Guihal <dguihal@gmail.com>
2026-02-11 17:54:20 +0100

    Linting fixes

--- goboard.go
+++ goboard.go
@@ -68,47 +68,18 @@ func init() {
 	flag.BoolVar(&showHelp, "h", false, usageH+" (shorthand)")
 }
 
-func main() {
-	flag.Parse()
+func loadConfig(path string) (*Config, error) {
+	log.Printf("Using %s as config file\n", path)
 
-	println(configFilePath)
-	println(showHelp)
-
-	if showHelp {
-		flag.PrintDefaults()
-		os.Exit(0)
-	}
-
-	log.Printf("Using %s as config file\n", configFilePath)
-
-	configData, err := os.ReadFile(configFilePath)
+	configData, err := os.ReadFile(path)
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		return nil, fmt.Errorf("could not read config file %s: %w", path, err)
 	}
 
 	var config Config
 	err = yaml.Unmarshal(configData, &config)
 	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
-	// Manage access log file
-	if config.AccessLogFileMode == 0 {
-		config.AccessLogFileMode = 0660
-	}
-	var fiAccessLog *os.File
-	if len(config.AccessLogFile) > 0 {
-		fiAccessLog, err = os.OpenFile(config.AccessLogFile, os.O_RDWR|os.O_APPEND, config.AccessLogFileMode)
-		if err != nil {
-			fiAccessLog, err = os.Create(config.AccessLogFile)
-			if err != nil {
-				log.Printf("error: %v", err)
-				log.Printf("Fallbacking to stdout")
-				fiAccessLog = os.Stdout
-			}
-		}
-	} else {
-		fiAccessLog = os.Stdout
+		return nil, fmt.Errorf("could not parse config file %s: %w", path, err)
 	}
 
 	// Set some failsafe defaults
@@ -118,14 +89,70 @@ func main() {
 	if config.MaxHistorySize <= 0 {
 		config.MaxHistorySize = 30
 	}
-
-	// Open database
-	db, err := bolt.Open(config.GoBoardDBFile, config.GoBoardDBFileMode, &bolt.Options{Timeout: 1 * time.Second})
-	if err != nil {
-		log.Fatalf("error: %v", err)
+	if config.AccessLogFileMode == 0 {
+		config.AccessLogFileMode = 0660
 	}
 
-	// Initialize router
+	return &config, nil
+}
+
+func setupLogging(logFile string, mode os.FileMode) *os.File {
+	var fiAccessLog *os.File
+	var err error
+	if len(logFile) > 0 {
+		fiAccessLog, err = os.OpenFile(logFile, os.O_RDWR|os.O_APPEND, mode)
+		if err != nil {
+			fiAccessLog, err = os.Create(logFile)
+			if err != nil {
+				log.Printf("error creating log file %s: %v", logFile, err)
+				log.Printf("Fallbacking to stdout")
+				return os.Stdout
+			}
+		}
+	} else {
+		fiAccessLog = os.Stdout
+	}
+	return fiAccessLog
+}
+
+func setupSwagger(r *mux.Router, templateHandler *TemplateHandler, swaggerPath string) {
+	if len(swaggerPath) == 0 {
+		return
+	}
+	// Sanity checks before enabling swagger capability
+	realPath := os.ExpandEnv(swaggerPath)
+	if fi, err := os.Stat(realPath); os.IsNotExist(err) || !fi.IsDir() {
+		log.Println(realPath, "Not found or not a directory: Disabling swagger capabilities")
+	} else if _, err := os.Stat(strings.Join([]string{realPath, "/index.html"}, "")); os.IsNotExist(err) {
+		log.Println(strings.Join([]string{realPath, "/index.html"}, ""), "Not found: Disabling swagger capabilities")
+	} else {
+		templateHandler.SetSwaggerBaseDir(realPath)
+		swaggerOp := templateHandler.GetSwaggerOp()
+		r.HandleFunc(swaggerOp.RestPath, swaggerOp.handler).Methods(swaggerOp.Method)
+		r.PathPrefix("/swagger/").Handler(http.StripPrefix("/swagger/", http.FileServer(http.Dir(realPath))))
+		r.Handle("/swagger", http.RedirectHandler("/swagger/", http.StatusMovedPermanently))
+	}
+}
+
+func setupWebui(r *mux.Router, templateHandler *TemplateHandler, webuiPath string) {
+	if len(webuiPath) == 0 {
+		return
+	}
+	// Sanity checks before enabling webui capability
+	realPath := os.ExpandEnv(webuiPath)
+	if fi, err := os.Stat(realPath); os.IsNotExist(err) || !fi.IsDir() {
+		log.Println(realPath, "Not found or not a directory: Disabling webui capabilities")
+	} else if _, err := os.Stat(strings.Join([]string{realPath, "/index.html"}, "")); os.IsNotExist(err) {
+		log.Println(strings.Join([]string{realPath, "/index.html"}, ""), "Not found: Disabling webui capabilities")
+	} else {
+		templateHandler.setWebUIBaseDir(realPath)
+		r.PathPrefix("/webui/").Handler(http.StripPrefix("/webui/", http.FileServer(http.Dir(realPath))))
+		r.Handle("/webui", http.RedirectHandler("/webui/", http.StatusMovedPermanently))
+		r.Handle("/", http.RedirectHandler("/webui/", http.StatusMovedPermanently))
+	}
+}
+
+func setupRouter(db *bolt.DB, config *Config) *mux.Router {
 	mainRouter := mux.NewRouter().StrictSlash(true)
 	r := mainRouter
 
@@ -151,57 +178,58 @@ func main() {
 	}
 
 	templateHandler := NewTemplateHandler()
+	setupSwagger(r, templateHandler, config.SwaggerPath)
+	setupWebui(r, templateHandler, config.WebuiPath)
 
-	// Swagger operations
-	if len(config.SwaggerPath) > 0 {
+	return mainRouter
+}
 
-		// Sanity checks before enabling swagger capability
-		realPath := os.ExpandEnv(config.SwaggerPath)
-		if fi, err := os.Stat(realPath); os.IsNotExist(err) || !fi.IsDir() {
-			log.Println(realPath, "Not found or not a directory: Disabling swagger capabilities")
-		} else if _, err := os.Stat(strings.Join([]string{realPath, "/index.html"}, "")); os.IsNotExist(err) {
-			log.Println(strings.Join([]string{realPath, "/index.html"}, ""), "Not found: Disabling swagger capabilities")
-		} else {
-			templateHandler.SetSwaggerBaseDir(realPath)
-			swaggerOp := templateHandler.GetSwaggerOp()
-			r.HandleFunc(swaggerOp.RestPath, swaggerOp.handler).Methods(swaggerOp.Method)
-			r.PathPrefix("/swagger/").Handler(http.StripPrefix("/swagger/", http.FileServer(http.Dir(realPath))))
-			r.Handle("/swagger", http.RedirectHandler("/swagger/", http.StatusMovedPermanently))
+func main() {
+	flag.Parse()
 
-		}
+	if showHelp {
+		flag.PrintDefaults()
+		os.Exit(0)
 	}
 
-	// Webui operations
-	if len(config.WebuiPath) > 0 {
-
-		// Sanity checks before enabling webui capability
-		realPath := os.ExpandEnv(config.WebuiPath)
-		if fi, err := os.Stat(realPath); os.IsNotExist(err) || !fi.IsDir() {
-			log.Println(realPath, "Not found or not a directory: Disabling webui capabilities")
-		} else if _, err := os.Stat(strings.Join([]string{realPath, "/index.html"}, "")); os.IsNotExist(err) {
-			log.Println(strings.Join([]string{realPath, "/index.html"}, ""), "Not found: Disabling webui capabilities")
-		} else {
-			templateHandler.setWebUIBaseDir(realPath)
-			r.PathPrefix("/webui/").Handler(http.StripPrefix("/webui/", http.FileServer(http.Dir(realPath))))
-			r.Handle("/webui", http.RedirectHandler("/webui/", http.StatusMovedPermanently))
-			r.Handle("/", http.RedirectHandler("/webui/", http.StatusMovedPermanently))
-		}
+	config, err := loadConfig(configFilePath)
+	if err != nil {
+		log.Fatalf("error: %v", err)
 	}
+
+	// Manage access log file
+	fiAccessLog := setupLogging(config.AccessLogFile, config.AccessLogFileMode)
+	if fiAccessLog != os.Stdout {
+		defer fiAccessLog.Close()
+	}
+
+	// Open database
+	db, err := bolt.Open(config.GoBoardDBFile, config.GoBoardDBFileMode, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	defer db.Close()
+
+	// Initialize router
+	mainRouter := setupRouter(db, config)
 
 	fmt.Println("GoBoard version ", goBoardVer, " starting on port", config.ListenPort)
 
-	var handler http.Handler
-	if fiAccessLog != nil {
-		handler = handlers.LoggingHandler(fiAccessLog, mainRouter)
-	} else {
-		handler = mainRouter
+	handler := handlers.LoggingHandler(fiAccessLog, mainRouter)
+
+	server := &http.Server{
+		Addr:              fmt.Sprint(":", config.ListenPort),
+		Handler:           handler,
+		ReadHeaderTimeout: 20 * time.Second,
+		ReadTimeout:       1 * time.Minute,
+		WriteTimeout:      2 * time.Minute,
+		IdleTimeout:       5 * time.Minute,
 	}
 
-	server := &http.Server{Addr: fmt.Sprint(":", config.ListenPort), Handler: handler}
-
 	go func() {
-		if err := server.ListenAndServe(); err != nil {
-			log.Fatal(err)
+		log.Println("Server starting on", server.Addr)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe error: %v", err)
 		}
 	}()
 
@@ -217,7 +245,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 	if err := server.Shutdown(ctx); err != nil {
-		log.Fatal(err)
+		log.Printf("Server shutdown error: %v", err)
 	}
 }
 
